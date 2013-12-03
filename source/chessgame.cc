b7a7912301a265401676756cc71d01491da256d9
Winston <winxton@gmail.com>
2013-12-02 22:18:21 -0500

    fix checkmate and stalemate conditions

--- source/chessgame.cc
+++ source/chessgame.cc
@@ -14,37 +14,36 @@ ChessGame::ChessGame (
            blackPlayer(blackPlayer) {}
 
 void ChessGame::doTurn(const Player &player) {
-
-	Action *action = player.getAction(*currentState);
-
-	cerr << "INFO: Apply action: " << *action << endl;
-
-	// apply the action to the current state
-	// AND update the graphics
-	action->apply(*currentState, true, true);
-
-	delete action;
-
-	string oppositeColor = player.getColor() == "white" ? "black" : "white";
-
-	// check for stalemate
-	if ( !currentState->isUnderCheck(oppositeColor)
-		&& !currentState->hasLegalMoves(oppositeColor) ) 
+	// player has no legal moves
+    if (!currentState->hasLegalMoves(player.getColor())) 
 	{
-		currentState->setGameEnded(GameState::STALEMATE);
-	}
-	// other player cannot move -> lost
-	else if (!currentState->hasLegalMoves(oppositeColor)) 
-	{
+	    //player is under check
+	    if ( currentState->isUnderCheck(player.getColor()) ) {
 		if (player.getColor() == "white") {
-			currentState->setGameEnded(GameState::WHITE_WINS_BY_CHECKMATE);
-		} else {
 			currentState->setGameEnded(GameState::BLACK_WINS_BY_CHECKMATE);
+		} else {
+			currentState->setGameEnded(GameState::WHITE_WINS_BY_CHECKMATE);
 		}
-	}
+	    }
+	    // not under check -> stalemate
+	    else {
+		currentState->setGameEnded(GameState::STALEMATE);
+	    }
+	} else {
 
-	currentState->swapTurns();
-	currentState->printBoard();
+	    Action *action = player.getAction(*currentState);
+
+	    cerr << "INFO: Apply action: " << *action << endl;
+
+	    // apply the action to the current state
+	    // AND update the graphics
+	    action->apply(*currentState, true, true);
+
+	    delete action;
+
+	    currentState->swapTurns();
+	    currentState->printBoard();
+	}
 
 	if (currentState->isGameEnded()) {
 		if (currentState->getGameResult() == GameState::WHITE_WINS_BY_CHECKMATE) {
